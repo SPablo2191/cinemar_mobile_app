8f43bd8fc6c1455ec214f455d12447792ac28425
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-02-25 13:38:20 -0300

    feat: swiper added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,10 +7,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cinemar'),
+        appBar: AppBar(
+          title: const Text('Cinemar'),
+          backgroundColor: Colors.blueAccent,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+          ],
+        ),
+        body: Column(
+          children: [_cardSwiper()],
+        ));
+  }
+
+  _cardSwiper() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.only(top: 10.0),
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "https://picsum.photos/350/150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: const SwiperPagination(),
+        control: const SwiperControl(),
       ),
-      body: Center(child: Text('Bienvenido a cinemar!')),
     );
   }
 }
