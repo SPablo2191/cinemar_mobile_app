36231c74ba59ced14d13bb73478f53e3db000108
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-03-05 13:58:11 -0300

    feat: google fonts added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:cinemar_mobile_app/src/providers/movie_provider.dart';
 import 'package:cinemar_mobile_app/src/widgets/card_swiper_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: AppBar(
-          title: const Text('Cinemar'),
-          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          title: Text(
+            'Cinemar',
+            style: GoogleFonts.dmSans(
+                fontSize: 20, color: Colors.black, fontWeight: FontWeight.w800),
+          ),
+          backgroundColor: Colors.white.withAlpha(200),
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ))
           ],
         ),
         body: Column(
-          children: [_cardSwiper()],
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            _cardSwiper()
+          ],
         ));
   }
 
   _cardSwiper() {
     // in order to return async data => future builder
     final MovieProvider movieProvider = MovieProvider();
-    print(movieProvider.getNowPlaying());
     return FutureBuilder(
       future: movieProvider.getNowPlaying(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
